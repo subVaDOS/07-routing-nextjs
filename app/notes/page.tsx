e81bf2ab09fa1a4c81dc4969d065a5c8306d3602
Manokha Vadim <manokha.vadim@gmail.com>
2025-08-09 00:54:56 +0300

    modified

--- app/notes/page.tsx
+++
@@ -1,8 +0,0 @@
-import { fetchNotes } from '@/lib/api';
-
-import Notes from './Notes.client';
-
-export default async function Page() {
-  const data = await fetchNotes('', 1);
-  return <Notes initialData={data} />;
-}
