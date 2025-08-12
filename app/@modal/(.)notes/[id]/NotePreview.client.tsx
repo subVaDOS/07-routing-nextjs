6172b056df0749e9cba9e28253f224e2be78514b
Manokha Vadim <manokha.vadim@gmail.com>
2025-08-12 13:41:40 +0300

    mod del

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,68 +1,3 @@
-// "use client";
-// import { useQuery } from "@tanstack/react-query";
-// import { useParams, useRouter } from "next/navigation";
-// import { fetchNoteById } from "@/lib/api";
-// import css from "./NotePreview.module.css";
-// import Modal from "@/components/Modal/Modal";
-// import { Note } from "@/types/note";
-// import { FiX } from "react-icons/fi";
-
-// const NotePreview: React.FC = () => {
-//   const router = useRouter();
-//   const { id } = useParams<{ id: string }>();
-
-//   const {
-//     data: note,
-//     isLoading,
-//     error,
-//   } = useQuery<Note>({
-//     queryKey: ["note", id],
-//     queryFn: () => fetchNoteById(id),
-//     refetchOnMount: false,
-//   });
-
-//   const handleClose = () => {
-//     router.back();
-//   };
-
-//   if (isLoading) return <p>Loading, please wait...</p>;
-//   if (error) return <p>Error: {error.message}</p>;
-//   if (!note) return <p>Note not found</p>;
-
-//   return (
-//     <Modal>
-//       <div className={css.container}>
-//         <button
-//           className={css.closeButton}
-//           onClick={handleClose}
-//           aria-label="Close modal"
-//         >
-//           <FiX size={24} />
-//         </button>
-
-//         <div className={css.item}>
-//           <div className={css.header}>
-//             <h2>{note.title}</h2>
-//           </div>
-
-//           <div className={css.contentSection}>
-//             <p className={css.content}>{note.content}</p>
-//           </div>
-
-//           <div className={css.meta}>
-//             <span className={css.tag}>{note.tag}</span>
-//             <span className={css.date}>
-//               {new Date(note.createdAt).toLocaleDateString()}
-//             </span>
-//           </div>
-//         </div>
-//       </div>
-//     </Modal>
-//   );
-// };
-
-// export default NotePreview;
-
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import { useParams, useRouter } from "next/navigation";
@@ -86,6 +21,7 @@ const NotePreview: React.FC = () => {
   const handleClose = () => {
     router.back();
   };
+
   const router = useRouter();
   if (isLoading) return <p>Loading, please wait...</p>;
   if (error || !note) return <p>Something went wrong.</p>;
